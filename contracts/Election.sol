8633e89a08d6d7307931fa76c0179c21d2eb87a7
Asutosh <asutosh407@gmail.com>
2018-09-26 23:52:28 +0530

    candidate added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,23 @@
 pragma solidity ^0.4.2;
 
-contract Election{
-    //Store Candidate
-    string public candidate;
-    //Read Candidate
-    //Constructor
-    function Election() public{
-        candidate="Candidate 1";
+contract Election {
+   //Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
     }
-
+   //Store canidates
+   //Fetch candidate
+    mapping(uint=>Candidate) public candidates;
+      //Store candidate Count
+    uint public candidateCount;
+    function Election() public{
+        addCandidate("Ram Singh");
+        addCandidate("Hari Bhai");
+    }
+    function addCandidate(string _name) private {
+        candidateCount++;
+        candidates[candidateCount]=Candidate(candidateCount,_name,0);
+    }
 }
