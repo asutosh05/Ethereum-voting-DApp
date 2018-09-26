dd4cc2a05a9e42b9c4f3bef05931942e011763e8
Asutosh <asutosh407@gmail.com>
2018-09-26 22:45:39 +0530

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.11;
+
+contract Election{
+    //Store Candidate
+    string public candidate;
+    //Read Candidate
+    //Constructor
+    function Election() public{
+        candidate="Candidate 1";
+    }
+
+}
